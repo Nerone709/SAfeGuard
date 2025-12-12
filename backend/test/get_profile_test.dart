68c8768ca85b1b45f74ae7d9ff85c5c9c11bc717
AntoCast <antonellocastelluccio04@gmail.com>
2025-12-12 21:12:16 +0100

    Aggiunta di un quinto test che controlla se ad una chiamata di get profile viene passata anche la password. Aggiunta di un sesto test che controlla che tipo di utente ritorna il metodo se mettiamo isSoccorritore a true ma con un email non istituzionale. Tutti e 6 i test hanno avuto successo.

--- backend/test/get_profile_test.dart
+++ backend/test/get_profile_test.dart
@@ -20,7 +20,7 @@ void main() {
 
   group('ProfileService - getProfile', () {
 
-    // Scenario 1: get di un profilo soccorritore
+    // [TU_PRO_1] Scenario 1: get di un profilo soccorritore
     test('Deve restituire un oggetto Soccorritore e rimuovere la password', () async {
       // 1. ARRANGE
       final int soccorritoreId = 10;
@@ -46,7 +46,7 @@ void main() {
       verify(mockRepository.findUserById(soccorritoreId)).called(1);
     });
 
-    // Scenario 2: get di un profilo cittadino
+    // [TU_PRO_2] Scenario 2: get di un profilo cittadino
     test('Deve restituire un oggetto Utente standard se l\'email è normale', () async {
       // 1. ARRANGE
       final int userId = 20;
@@ -71,7 +71,7 @@ void main() {
     });
 
 
-    // Scenario 3: utente non trovato
+    // [TU_PRO_3] Scenario 3: utente non trovato
     test('Deve restituire null se l\'utente non esiste', () async {
       // 1. ARRANGE
       when(mockRepository.findUserById(999)).thenAnswer((_) async => null);
@@ -83,7 +83,7 @@ void main() {
       expect(result, isNull);
     });
 
-    // Scenario 4: restituzione corretta dei dati
+    // [TU_PRO_4] Scenario 4: restituzione corretta dei dati
     test('Deve restituire i dati giusti (nome e cognome)', () async {
       // 1. ARRANGE
       final int soccorritoreId = 10;
@@ -111,6 +111,63 @@ void main() {
       expect(result?.email, emailSoccorritore);
 
       verify(mockRepository.findUserById(soccorritoreId)).called(1);
+    });
+
+    // [TU_PRO_5] Scenario 5: restituzione corretta dei dati e non la password
+    test('Deve restituire i dati giusti ma non la password', () async {
+      // 1. ARRANGE
+      final int soccorritoreId = 10;
+
+      final emailSoccorritore = 'mario@crocerossa.it';
+
+      final rawData = {
+        'id': soccorritoreId,
+        'email': emailSoccorritore,
+        'nome': 'Mario',
+        'cognome': 'Rossi',
+        'passwordHash': '1234567890A!'
+      };
+
+      when(mockRepository.findUserById(soccorritoreId))
+          .thenAnswer((_) async => Map<String, dynamic>.from(rawData));
+
+      // 2. ACT
+      final result = await service.getProfile(soccorritoreId);
+
+      // 3. ASSERT
+      expect(result, isNotNull);
+      expect(result?.nome, 'Mario');
+      expect(result?.cognome, 'Rossi');
+      expect(result, isA<Soccorritore>());
+      expect(result?.email, emailSoccorritore);
+      expect(result?.passwordHash, isNot('1234567890A!'));
+      expect(result?.passwordHash, isNot('5c5456f6162d6fe5f18eb7636b1efe8b4506028c4a5f20df888476fbf994d259')); // SHA 256 di '1234567890A!'
+      verify(mockRepository.findUserById(soccorritoreId)).called(1);
+    });
+
+    // [TU_PRO_6] Scenario 6: setting della flag isSoccorritore a true mentre l'email non è istituzionale
+    test('isSoccorritore settato a true mentre l\'email non è di tipo soccorritore', () async {
+      // 1. ARRANGE
+      final userId = 30;
+      final emailCittadino = 'hacker@gmail.com';
+
+      final rawData = {
+        'id': userId,
+        'email': emailCittadino,
+        'nome': 'Hacker',
+        'isSoccorritore': true,
+        'passwordHash': 'x'
+      };
+
+      when(mockRepository.findUserById(userId))
+          .thenAnswer((_) async => Map<String, dynamic>.from(rawData));
+
+      // 2. ACT
+      final result = await service.getProfile(userId);
+
+      // 3. ASSERT
+      expect(result, isA<Utente>());
+      expect(result, isNot(isA<Soccorritore>()));
     });
   });
 }
