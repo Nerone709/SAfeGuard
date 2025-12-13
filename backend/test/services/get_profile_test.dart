62b90bf05ec954552999eec88db2ac769673003c
jlgionny <giovannilamberti24@gmail.com>
2025-12-13 19:33:18 +0100

    Fix test

--- backend/test/services/get_profile_test.dart
+++ backend/test/services/get_profile_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:backend/services/profile_service.dart';
